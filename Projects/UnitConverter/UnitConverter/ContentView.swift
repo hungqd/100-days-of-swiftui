63465f5edf6ef5d3f3c01379d2d05a1b1ef05edf
Đặng Quang Hưng <hungdq58@gmail.com>
2019-10-19 12:46:21 +0700

    Completed 1st challenge project

--- Projects/UnitConverter/UnitConverter/ContentView.swift
+++ Projects/UnitConverter/UnitConverter/ContentView.swift
@@ -8,9 +8,81 @@
 
 import SwiftUI
 
+private func value(input: Double) -> Double {
+    return input
+}
+
+private func celsius2Fahrenheit(input: Double) -> Double {
+    return input * 9.0 / 5 + 32
+}
+
+private func celsius2Kelvin(input: Double) -> Double {
+    return input + 273.15
+}
+
+private func fahrenheit2Celsius(input: Double) -> Double {
+    return (input - 32) * (5.0 / 9)
+}
+
+private func fahrenheit2Kelvin(input: Double) -> Double {
+    return (input - 32) * 5.0 / 9 + 273.15
+}
+
+private func kelvin2Celsius(input: Double) -> Double {
+    return input - 273.15
+}
+
+private func kelvin2Fahrenheit(input: Double) -> Double {
+    return (input - 273.15) * 9.0 / 5 + 32
+}
+
+private let converters: [[(Double) -> Double]] = [
+    [value, celsius2Fahrenheit, celsius2Kelvin],
+    [fahrenheit2Celsius, value, fahrenheit2Kelvin],
+    [kelvin2Celsius, kelvin2Fahrenheit, value ]
+]
+
 struct ContentView: View {
+
+    private let units = ["Celsius", "Fahrenheit", "Kelvin"]
+
+    @State private var inputUnitIndex = 0
+    @State private var ouputUnitIndex = 1
+
+    @State private var inputValue = "37"
+
+    private func getOutputValue() -> Double {
+        guard let input = Double(inputValue) else {
+            return 0.0
+        }
+        return converters[inputUnitIndex][ouputUnitIndex](input)
+    }
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                HStack {
+                    TextField("From", text: $inputValue)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Text("°")
+                    Picker("", selection: $inputUnitIndex) {
+                        ForEach(0..<units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                }
+                HStack {
+                    Text("= \(getOutputValue(), specifier: "%.2f") °")
+                    Picker("", selection: $ouputUnitIndex) {
+                        ForEach(0..<units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Temperature Converter", displayMode: .inline)
+        }
     }
 }
 
