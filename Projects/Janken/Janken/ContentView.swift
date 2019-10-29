9d037e936424af34246fd987ad57f90c4956a766
Đặng Quang Hưng <hungdq58@gmail.com>
2019-10-29 10:23:19 +0700

    Completed day 25 challenge project

--- Projects/Janken/Janken/ContentView.swift
+++ Projects/Janken/Janken/ContentView.swift
@@ -9,8 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    private let icons = ["\u{270A}", "\u{270B}", "\u{270C}"]
+
+    @State private var appSelection = Int.random(in: 0..<3)
+    @State private var userSelection = -1
+    @State private var showResult = false
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.purple, .black]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Select your move")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                    HStack {
+                        ForEach(0..<icons.count) { selection in
+                            Button(action: {
+                                self.userSelection = selection
+                                self.showResult = true
+                            }) {
+                                Text(self.icons[selection])
+                                    .font(.system(size: 60))
+                            }
+                        }
+                    }
+                    Spacer()
+                }.navigationBarTitle(Text("Janken"), displayMode: .inline)
+                    .alert(isPresented: $showResult) { () -> Alert in
+                        var message = ""
+                        if userSelection == appSelection {
+                            message = "Draw!"
+                        } else if (userSelection == 0 && appSelection == 2)
+                            || (userSelection == 1 && appSelection == 0)
+                            || (userSelection == 2 && appSelection == 1) {
+                            message = "You won! Keep it up"
+                        } else {
+                            message = "You lose! Good luck"
+                        }
+                        return Alert(title: Text(""), message: Text(message), dismissButton: .default(Text("OK"), action: {
+                            self.appSelection = Int.random(in: 0..<3)
+                        }))
+                }
+            }
+        }
     }
 }
 
