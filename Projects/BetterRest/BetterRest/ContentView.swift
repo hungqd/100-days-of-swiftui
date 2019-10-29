ad84619b2310dea98fd3da73116cc18a0ff98407
Đặng Quang Hưng <hungdq58@gmail.com>
2019-10-29 11:11:14 +0700

    Initialize day 26 project

---
+++ Projects/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Đặng Quang Hưng on 10/29/19.
+//  Copyright © 2019 Đặng Quang Hưng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
